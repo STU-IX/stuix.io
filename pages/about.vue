88c1b508af6e4423513687a83965bc4fe043ae28
腹黒い茶 <haraguroicha@tipsy.coffee>
2020-10-18 03:31:23 +0800

    update to use nuxt framework and get members list from spreadsheet

---
+++ pages/about.vue
@@ -0,0 +1,25 @@
+<template lang="pug">
+div
+  h4 About
+  p This is a non-profit IX (Internet Exchange)
+  h4 Mission Statement
+  h4 Who are the network operators?
+  p
+  h4 Special Thanks
+  p
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      page: '/about',
+      title: 'About'
+    }
+  },
+  mounted() {
+    this.$store.commit('updatePage', this.page)
+    this.$store.commit('updateTitle', this.title)
+  }
+}
+</script>
---
+++ pages/members.vue
@@ -0,0 +1,57 @@
+<template>
+  <div>
+    <div id="members-container">
+      <table>
+        <thead>
+          <tr>
+            <th>AS</th>
+            <th>IPv4</th>
+            <th>IPv6</th>
+            <th>Country</th>
+            <th>NIC</th>
+            <th>Org</th>
+            <th>TG</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="(item, i) in members" :key="i">
+            <td>{{ item.ASN }}</td>
+            <td><v-icon v-show="item.IPv4">mdi-check-bold</v-icon></td>
+            <td><v-icon v-show="item.IPv6">mdi-check-bold</v-icon></td>
+            <td>{{ item.Country }}</td>
+            <td>{{ item.NIC }}</td>
+            <td>{{ item.Org }}</td>
+            <td>{{ item.OwnerTgId }}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>
+</template>
+
+<script>
+import '@mdi/font/css/materialdesignicons.css'
+import m from 'static/data/members.json'
+
+export default {
+  data() {
+    return {
+      page: '/members',
+      title: 'Members',
+      members: m
+    }
+  },
+  mounted() {
+    this.$store.commit('updatePage', this.page)
+    this.$store.commit('updateTitle', this.title)
+  }
+}
+</script>
+
+<style lang="stylus">
+th
+  font-weight bold
+th, td
+  padding-left  10px
+  padding-right 10px
+</style>
