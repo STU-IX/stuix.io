d674dd15839d3ff8ead78abcae6f7a03ebffc16a
Li-Heng Yu <007seadog@gmail.com>
2020-10-28 01:37:14 +0800

    Update about.vue

--- pages/about.vue
+++ pages/about.vue
@@ -1,8 +1,10 @@
 <template lang="pug">
 div
   h4 About
-  p This is a non-profit IX (Internet Exchange)
+  p This is a non-profit IX (Internet Exchange) for educational purposes.
+  p The full name of the IX is "Student &amp; Technology United Internet Exchanges".
   h4 Mission Statement
+  p 
   h4 Who are the network operators?
   p
   h4 Special Thanks
