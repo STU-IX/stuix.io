71c2be65231808f4eb4ca43d940b1dd5f6f14e3b
gnehs <jayuiop@gmail.com>
2020-12-28 22:13:48 +0800

    changed index layout

--- pages/index.vue
+++ pages/index.vue
@@ -1,26 +1,42 @@
 <template lang="pug">
 div
-  img(src="//raw.githubusercontent.com/STU-IX/branding/main/Logo/Logo.svg",width="256px")
-  br
-  br
-  p Student & Technology United Internet Exchanges
+	.lr-container
+		.l
+			img(
+				src="//raw.githubusercontent.com/STU-IX/branding/main/Logo/Logo.svg",
+				width="256px"
+			)
+		.r
+			p Student & Technology United Internet Exchanges
 
-  section.field-row(style='justify-content: flex-end')
-    a(href='/join/')
-      button Join STUIX!
+	section.field-row(style="justify-content: flex-end")
+		a(href="/join/")
+			button Join STUIX!
 </template>
-
+<style lang="sass">
+.lr-container
+  display: flex
+  .l
+  .r
+    padding: 0 20px
+    flex: 1
+@media screen and (max-width: 768px)
+  .lr-container
+    display: block
+    .l
+      text-align: center
+</style>
 <script>
 export default {
-  data() {
-    return {
-      page: '/',
-      title: 'Home'
-    }
-  },
-  mounted() {
-    this.$store.commit('updatePage', this.page)
-    this.$store.commit('updateTitle', this.title)
-  }
+	data() {
+		return {
+			page: '/',
+			title: 'Home'
+		}
+	},
+	mounted() {
+		this.$store.commit('updatePage', this.page)
+		this.$store.commit('updateTitle', this.title)
+	}
 }
 </script>
