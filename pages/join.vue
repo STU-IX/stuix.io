2070564930e092311712ff49ffc2b266490cda04
gnehs <jayuiop@gmail.com>
2020-12-28 22:13:24 +0800

    fixed typo

--- pages/join.vue
+++ pages/join.vue
@@ -1,40 +1,40 @@
 <template>
-  <div>
-    <form action="" method="post">
-      <div class="field-row-stacked" style="width: 200px">
-        <label for="ASN">ASN:</label>
-        <input type="text" id="ASN">
-      </div>
-      <div class="field-row-stacked" style="width: 200px">
-        <label for="Network_Name">Network Name:</label>
-        <input type="text" id="Network_Name">
-      </div>
-      <div class="field-row-stacked" style="width: 200px">
-        <label for="Email">Contact Email:</label>
-        <input type="email" id="Email">
-      </div>
-      <div class="field-row-stacked" style="width: 200px">
-        <label for="WebSite">WebSite (optional):</label>
-        <input type="text" id="WebSite">
-      </div>
-    </form>
-    <br>
-    <button type="submit">Submit</button>
-  </div>
+	<div>
+		<form action method="post">
+			<div class="field-row-stacked" style="width: 200px">
+				<label for="ASN">ASN:</label>
+				<input type="text" id="ASN" />
+			</div>
+			<div class="field-row-stacked" style="width: 200px">
+				<label for="Network_Name">Network Name:</label>
+				<input type="text" id="Network_Name" />
+			</div>
+			<div class="field-row-stacked" style="width: 200px">
+				<label for="Email">Contact Email:</label>
+				<input type="email" id="Email" />
+			</div>
+			<div class="field-row-stacked" style="width: 200px">
+				<label for="WebSite">Website (optional):</label>
+				<input type="text" id="WebSite" />
+			</div>
+		</form>
+		<br />
+		<button type="submit" disabled>Submit</button>
+	</div>
 </template>
 
 <script>
 export default {
-  data() {
-    return {
-      page: '/join/',
-      title: 'Join'
-    }
-  },
-  mounted() {
-    this.$store.commit('updatePage', this.page)
-    this.$store.commit('updateTitle', this.title)
-    Array.from(document.querySelectorAll('form .field-row-stacked input:not([type])')).forEach(e => e.setAttribute("type", "text"))
-  }
+	data() {
+		return {
+			page: '/join/',
+			title: 'Join'
+		}
+	},
+	mounted() {
+		this.$store.commit('updatePage', this.page)
+		this.$store.commit('updateTitle', this.title)
+		Array.from(document.querySelectorAll('form .field-row-stacked input:not([type])')).forEach(e => e.setAttribute("type", "text"))
+	}
 }
 </script>
