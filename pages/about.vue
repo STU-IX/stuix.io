cdae22a723844e09b8b29c13416b8b79578685ca
Sean Wei <me@sean.taipei>
2021-03-30 17:53:58 +0800

    Fix grammar

--- pages/about.vue
+++ pages/about.vue
@@ -3,12 +3,12 @@
 	h4 About
 	p This is a non-profit IX (Internet Exchange) for educational purposes.
 	p The full name of the IX is "Student &amp; Technology United Internet Exchanges".
-	p The IX is the first community based IX in Taiwan, and after few months of prepare, it is finally established in Jan 2021.
+	p The IX is the first community-based IX in Taiwan, and after few months of preparation, it is finally established in Jan 2021.
 	h4 Mission Statement
-	p The playground of networking player is restricted by the amount of cost to participate in the professional networking community in Taiwan.
-	p Therefore, creating a cheaper environment for networking player in Taiwan is our goal, which is a motivation for enhancing the communication between networking players, providing technical communication and educating the younger generation for participate in the networking operating field.
+	p The playground of networking players is restricted by the amount of cost to participate in the professional networking community in Taiwan.
+	p Therefore, creating a cheaper environment for networking players in Taiwan is our goal, which is a motivation for enhancing the communication between networking players, providing technical communication and educating the younger generation to participate in the networking operating field.
 	h4 Who are the network operators?
-	p Currently,
+	p Currently, 
 		|
 		a(href="https://seadog007.me", target="_blank") @seadog007
 		|
