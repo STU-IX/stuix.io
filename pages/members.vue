661320ba390fbf8a29f4bcf18d759f5c8180d89e
gnehs <jayuiop@gmail.com>
2021-11-04 09:34:54 +0800

    added MANRS tag

--- pages/members.vue
+++ pages/members.vue
@@ -1,40 +1,36 @@
 <template>
-	<div id="members-container" class="container">
-		<b-table borderless striped :items="members" :fields="fields">
-			<template #cell(IPv4)="data">
-				<span v-show="data.value">✓</span>
-			</template>
-			<template #cell(IPv6)="data">
-				<span v-show="data.value">✓</span>
-			</template>
-		</b-table>
-	</div>
+  <div id="members-container" class="container">
+    <b-table borderless striped :items="members" :fields="fields">
+      <template #cell(IPv4)="data">
+        <span v-show="data.value">✓</span>
+      </template>
+      <template #cell(IPv6)="data">
+        <span v-show="data.value">✓</span>
+      </template>
+      <template #cell(Org)="data">
+        <span>{{ data.value }} <span v-show="data.item.MANRS" class="tag">MANRS</span></span>
+      </template>
+    </b-table>
+  </div>
 </template>
 
-<script> 
+<script>
 import m from 'static/data/members.json'
 
 export default {
-	layout: ({ layout }) => layout,
-	data() {
-		return {
-			page: '/members/',
-			title: 'Members',
-			members: m,
-			fields: [
-				'ASN',
-				'IPv4',
-				'IPv6',
-				'Country',
-				'Org',
-				'Status'
-			],
-		}
-	},
-	mounted() {
-		this.$store.commit('updatePage', this.page)
-		this.$store.commit('updateTitle', this.title)
-	}
+  layout: ({ layout }) => layout,
+  data() {
+    return {
+      page: '/members/',
+      title: 'Members',
+      members: m,
+      fields: ['ASN', 'IPv4', 'IPv6', 'Country', 'Org', 'Status']
+    }
+  },
+  mounted() {
+    this.$store.commit('updatePage', this.page)
+    this.$store.commit('updateTitle', this.title)
+  }
 }
 </script>
 
@@ -44,4 +40,17 @@ export default {
 	overflow-x: scroll
 	table
 		min-width: 700px
+		.tag
+			--color: #008f00
+			border-radius: 100em
+			padding: 0.2em 0.5em
+			font-size: 0.8em
+			border: 1px solid var(--color)
+			color: var(--color)
+
+			.win95 &
+				border-radius: 0
+				background-color: navy
+				color: #fff
+				border: 1px solid #ccc
 </style>
