d008dfc5ee78eaffd6ad9ed736669156911b683a
Jasper Yu <007seadog@gmail.com>
2024-04-09 16:41:30 +0800

    Changed spreadsheet schema

--- download.sh
+++ download.sh
@@ -5,10 +5,8 @@ curl -s --url "https://sheets.googleapis.com/v4/spreadsheets/${SPREADSHEET_ID}?i
     jq "$(curl -s https://www.manrs.org/wp-json/manrs/v1/asn)"' as $manrs | .sheets[] | select(.properties.sheetId == '${SHEET_ID}') | select(.data[] != null) | .data[].rowData[] |
     select(.values[0].formattedValue != null and .values[0].formattedValue != "ASN") | {
     "ASN": .values[0].formattedValue,
-    "IPv4": (if .values[1].formattedValue == "TRUE" then true else false end),
-    "IPv6": (if .values[2].formattedValue == "TRUE" then true else false end),
-    "Country": .values[3].formattedValue,
-    "Org": .values[4].formattedValue,
-    "Status": .values[6].formattedValue,
+    "Country": .values[1].formattedValue,
+    "Org": .values[2].formattedValue,
+    "Status": .values[3].formattedValue,
     "MANRS": .values[0].formattedValue | tonumber | IN($manrs[])
 }' | jq -s '.' > static/data/members.json
